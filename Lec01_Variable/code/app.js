56740c6b0d8e3a0ebadb5f140092e4f28852cac0
Minh Thư Vũ <devminhthu2023@gmail.com>
2021-06-13 16:49:42 +0700

    JS, JS DOM & JS OOP

---
+++ Lec01_Variable/code/app.js
@@ -0,0 +1,24 @@
+/* Variable */
+var string = "Hello Guy";
+var number = 68;
+var boolean = true;
+var arr = ["apple", "orange"];
+var undefine;
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(arr);
+console.log(undefine);
+
+/* Constant
+- Cách định nghĩa hàng số (const)
+- Hằng số không được thay đổi giá trị.
+*/
+const PI = 3.14;
+var banKinh = 10;
+var dienTich = banKinh * banKinh * PI;
+var chuVi = banKinh * 2 * PI;
+
+console.log(dienTich);
+console.log(chuVi);
