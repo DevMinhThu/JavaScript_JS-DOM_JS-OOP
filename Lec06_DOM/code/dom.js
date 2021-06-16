e503abc39f9976841dc3c6e4bec51ff63ea24a9a
Minh Thư Vũ <devminhthu2023@gmail.com>
2021-06-16 10:00:24 +0700

    commit

---
+++ Lec06_DOM/code/dom.js
@@ -0,0 +1,10 @@
+// test luu tru data la mot Object
+var test = document.getElementById("user");
+
+console.log(test);
+
+/* 
+- Thẻ đang lấy là thẻ input
+- Id: user
+- Trả về toàn bộ thuộc tính của thẻ đang được lấy.
+*/
